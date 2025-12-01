a8d9bc1f383b0af1ceb3eee9a1911d0bf3d60810
hcamposxx <hector.campos16@inacapmail.cl>
2025-12-01 01:00:39 -0300

    otro cambio1

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -3,22 +3,21 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ProductTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
+    use RefreshDatabase;
+
     public function test_product_can_be_created()
-{
-    $response = $this->post('/api/products', [
-        'name' => 'Laptop',
-        'price' => 1000
-    ]);
+    {
+        $response = $this->postJson('/api/products', [
+            'name' => 'Laptop',
+            'price' => 1000,
+            'description' => 'test product'
+        ]);
 
-    $response->assertStatus(201);
+        $response->assertStatus(201);
+    }
 }
 
-}
